f5109e29b5862151c66882d2029b68dfebc9cbed
rafiullah786 <rafiullah78@gmail.com>
2019-05-23 01:30:38 +0200

    Comments added for path settings

---
+++ Exract7zip/Exract7zip/Program.cs
@@ -0,0 +1,35 @@
+﻿using SevenZip;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exract7zip
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Giver the full path with extension to the file 
+            string vmicFilePath = @"C:\E Drive\Precipoint Coding Chellange\Testing\Rattus norvegicus - Kidney.vmic";
+            VmicFileExtractor vmic = new VmicFileExtractor();
+            bool dziFolderpath= vmic.ExtractVmicFile(vmicFilePath);
+
+            if (dziFolderpath == false)
+                Console.WriteLine("No file found");
+            else
+            {
+                
+                int pos = vmicFilePath.LastIndexOf(@"\") + 1;
+                string folderPath = vmicFilePath.Substring(0, pos);
+
+                folderPath=folderPath + "dzc_output_files";
+                Console.WriteLine(folderPath);
+            }
+               
+
+        }
+    }
+}
---
+++ Exract7zip/Exract7zip/VmicFileExtractor.cs
@@ -0,0 +1,56 @@
+﻿using SevenZip;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exract7zip
+{
+    public class VmicFileExtractor
+    {
+        int totalCount = 0;
+
+
+        public bool ExtractVmicFile(string path)
+        {
+            try
+            {
+                string extractFrom = path;
+                int pos = extractFrom.LastIndexOf(@"\") + 1;
+                string extractTo = extractFrom.Substring(0, pos);
+
+                if (Environment.Is64BitOperatingSystem)
+                {
+                    //Add the address to the SevenZip dll files
+                    SevenZipExtractor.SetLibraryPath(@"C:\Users\sami\Downloads\sevenzipsharp\sourceCode\sourceCode\SevenZip\7z64.dll");
+                }
+                SevenZipExtractor extractor = new SevenZipExtractor(extractFrom);
+                extractor.ExtractArchive(extractTo);
+
+
+                string extractFromImage = extractTo + @"Image.vmici";
+
+                
+                if (totalCount > 0)
+                {
+                    
+                    return true ;
+                }
+                    
+                
+                totalCount++;
+                ExtractVmicFile(extractFromImage);
+
+                return true;
+
+            }
+
+            catch(Exception ex)
+            {
+                return false;
+            }
+
+        }
+    }
+}
